d9bacaabef4de05a840a0d97d2265cc397ec4532
Lawrence <qq19907734768@gmail.com>
2024-03-23 16:22:32 +0800

    first commit

---
+++ src/page/Home/index.js
@@ -0,0 +1,10 @@
+const Home = ()=>
+{
+    return (
+        <div>
+            Home我是登录页
+        </div>
+    );
+}
+   
+export default Home
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Home from "../page/Home/index";
+import Login from "../page/Login/index";
+import { createBrowserRouter } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+]);
+
+export default router;
